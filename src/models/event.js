01e2376284b0528ec0f458e13a280d5e20ba8ceb
CherutoMercy <m.thirty100@gmail.com>
2017-03-26 14:58:18 +0300

    add multiple event objects

--- src/models/event.js
+++ src/models/event.js
@@ -1,6 +1,21 @@
-const Event = {
-	id   :"0",
-	title:"Pair programming",
-	description:"A pair of programmers learning nodejs",
-	date:"20/03/2017"
+// const Event = {
+// 	id   :"0",
+// 	title:["Pair programming","Bucharest Nodejs"]
+// 	description:"A pair of programmers learning nodejs",
+// 	date:["20/03/2017","21/03/2017"]
+// };
+
+let Event = class Event {
+  constructor(id, title, description, date) {
+    this.id = id;
+    this.title = title;
+	this.description = description;
+	this.date = date;
+  }
 };
+
+event1 = new Event("0", "Pair programming", "A pair of programmers learning nodejs", "21/03/2017");
+event2 = new Event("1", "Nodejs meetup", "Javascript geeks", "12/04/2017");
+
+const events = [event1, event2];
+console.log(events);
