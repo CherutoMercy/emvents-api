5cc7fd0f1cf076232bd73b438daa25719c807951
teodora.dima <teo.dima@gmail.com>
2017-04-01 10:07:07 +0300

    req #5 alternative way of adding new event

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app     = express();
-const model  = require('./src/models/event');
+const model   = require('./src/models/event');
 
 const getAllEvents = (req, res) => {
 	res.json(model);
@@ -16,41 +16,28 @@ const getEventsById = (req, res) => {
     res.json(result);
 }
 
-//could not get this from model
-let Event = class Event {
-  constructor(id, title, description, date) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.date = date;
-  }
-};
-
-
-//create new event
-const PostEvent =(req, res) => { 
-    const event = new Event();
-        event.id= "3";
-        event.title= "New Event";
-        event.description= "Hope this event will be created";
-        event.date= "1/4/2017";
-    console.log("event at this stage", event);
-    const events = [event1, event2 ,event];
-    res.json(events);
+const postEvent = (req, res) => { 
+    const event = {
+        id: "2",
+        title: "New Event",
+        description: "Hope this event will be created",
+        date: "1/4/2017"
+    }
+    model.push(event);
+    res.json(model);
 }
 
 app.route('/events')
-	.get(getAllEvents);
+	.get(getAllEvents)
+    .post(postEvent);
 
 app.route('/events/:id')
     .get(getEventsById);
 
-app.route('/create')
-    .post(PostEvent);
-
 app.get('/', (req, res) => {
         res.send('Get Available Events!');
     });
+
 app.listen(3000, () => {
     console.log('Awesome server running on port 3000');
 });
